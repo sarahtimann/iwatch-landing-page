1a363ed3e748603e2b8102ceba6421b3ebeb5e38
Sarah Timann <saol0003@stud.kea.dk>
2025-11-04 15:30:11 +0100

    Refactor components and styles for improved layout and design; update Arrows, BuyNowButton, GalleryOptions, Header, and global styles

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -4,18 +4,20 @@ import { LuShoppingBag } from "react-icons/lu";
 
 const Header = () => {
   return (
-    <header>
-      <div className="apple-icon">
+    <header className="mr-40 flex items-center justify-between">
+      <div className="text-3xl text-white">
         <SiApple />
       </div>
-      <div className="header-center">
+      <div className="flex items-center gap-18">
         <h3>Mac</h3>
         <h3>iPhone</h3>
         <h3>iPod</h3>
-        <h3 className="iwatch-text">iWatch</h3>
+        <h3 className="rounded-4xl bg-stone-50 px-[20px] py-[5px] text-[#b6ccda] outline outline-1 outline-white">
+          iWatch
+        </h3>
         <h3>Support</h3>
       </div>
-      <div className="header-icons">
+      <div className="flex gap-4 text-[20px] text-white">
         <IoSearch />
         <LuShoppingBag />
       </div>
